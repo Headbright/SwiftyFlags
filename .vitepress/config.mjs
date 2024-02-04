5300a51e6e6cfe4cab7a11a808237aaf21bb1a49
Konstantin Kostov <konstantin@headbright.be>
2024-02-04 10:53:44 +0100

    Layout improvements

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -8,16 +8,64 @@ const versions = ["5.9", "5.8", "5.7", "5.6"].map((v) => {
 export default defineConfig({
   title: "SwiftyFlags",
   description: "List of Swift meta-programming language features",
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     // search: {
     //   provider: "local",
     // },
+    logo: "/logo.png",
+    lastUpdated: true,
+    cleanUrls: true,
     nav: [
       { text: "Home", link: "/" },
       // { text: 'Examples', link: '/markdown-examples' }
     ],
 
+    // search: {
+    //   provider: 'algolia',
+    //   options: {
+    //     appId: '...',
+    //     apiKey: '...',
+    //     indexName: '...'
+    //   }
+    // },
     sidebar: [
       {
         text: "by Swift Version",
--- index.md
+++ index.md
@@ -16,6 +16,9 @@ hero:
     - theme: alt
       text: Swift 5.7
       link: /language-features/5.7
+  image:
+    src: /swiftyflags.webp
+    alt: SwiftyFlags
 features:
   - title: About
     details: This page was created as a reference listing all available compiler flags for upcoming and experimental features. Stable features are also listed per toolchain version.
@@ -24,3 +27,25 @@ features:
 #   - title: Feature C
 #     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
+
+<style>
+:root {
+  --vp-home-hero-name-color: transparent;
+  --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe 30%, #41d1ff);
+
+  --vp-home-hero-image-background-image: linear-gradient(-45deg, #bd34fe 50%, #47caff 50%);
+  --vp-home-hero-image-filter: blur(44px);
+}
+
+@media (min-width: 640px) {
+  :root {
+    --vp-home-hero-image-filter: blur(56px);
+  }
+}
+
+@media (min-width: 960px) {
+  :root {
+    --vp-home-hero-image-filter: blur(68px);
+  }
+}
+</style>
